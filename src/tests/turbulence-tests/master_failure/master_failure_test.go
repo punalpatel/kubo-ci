ed7ab0ee4e68f322e52890f5d4ddc9f07bc2f2e7
Brenda Chan <brchan@pivotal.io>
2017-12-21 16:12:29 -0500

    Fixes test for new bosh job names

--- src/tests/turbulence-tests/master_failure/master_failure_test.go
+++ src/tests/turbulence-tests/master_failure/master_failure_test.go
@@ -26,7 +26,7 @@ var _ = Describe("A single master and etcd failure", func() {
 		director := NewDirector(testconfig.Bosh)
 		deployment, err := director.FindDeployment(testconfig.Bosh.Deployment)
 		Expect(err).NotTo(HaveOccurred())
-		countRunningApiServerOnMaster := test_helpers.CountProcessesOnVmsOfType(deployment, test_helpers.MasterVmType, "kube-apiserver", test_helpers.VmRunningState)
+		countRunningApiServerOnMaster := CountProcessesOnVmsOfType(deployment, MasterVmType, "kube-apiserver", VmRunningState)
 
 		Expect(countRunningApiServerOnMaster()).To(Equal(1))
 
