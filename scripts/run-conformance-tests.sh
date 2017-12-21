7c88528a6b0fa5d731c71e781e4079e1719d9cda
Brenda Chan <brchan@pivotal.io>
2017-12-21 16:53:33 -0500

    Updates conformance script to use test config
    [#152770162]

--- scripts/run-conformance-tests.sh
+++ scripts/run-conformance-tests.sh
@@ -5,30 +5,40 @@
 set -eu
 set -o pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$PWD/git-kubo-ci/scripts/lib/environment.sh"
 
-. "$DIR/lib/environment.sh"
-. "$DIR/lib/run_test_suite.sh"
+GIT_KUBO_CI=$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)
+export GOPATH="$GIT_KUBO_CI"
+export DEPLOYMENT_NAME=${DEPLOYMENT_NAME:="ci-service"}
 
-copy_state_and_creds() {
+setup_env_dir() {
   cp "$PWD/gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
   cp "kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
+}
+
+set_kubeconfig() {
   "$PWD/git-kubo-deployment/bin/set_kubeconfig" "${KUBO_ENVIRONMENT_DIR}" "${DEPLOYMENT_NAME}"
 }
 
-if [ -z ${LOCAL_DEV+x} ] || [ "$LOCAL_DEV" != "1" ]; then
-  copy_state_and_creds
-fi
+generate_testconfig() {
+  "$GIT_KUBO_CI/scripts/generate-test-config.sh" "${KUBO_ENVIRONMENT_DIR}" "${DEPLOYMENT_NAME}"
+}
 
-if [ -z ${CONFORMANCE_RESULTS_DIR+x} ]; then
-  echo "Error: CONFORMANCE_RESULTS_DIR is not set, exiting..."
-  exit 1
-fi
+main() {
+  setup_env_dir
+  set_kubeconfig
+  generate_testconfig > "$PWD"/testconfig.json
 
-GOPATH="$KUBO_CI_DIR"
-export GOPATH
-export PATH_TO_KUBECONFIG="$HOME/.kube/config"
-export CONFORMANCE_RESULTS_DIR="$PWD/$CONFORMANCE_RESULTS_DIR"
-export RELEASE_TARBALL="$PWD/$KUBO_DEPLOYMENT_DIR/kubo-release.tgz"
+  export CONFIG="$PWD"/testconfig.json
 
-kubo::tests::run_test_suite "$GOPATH/src/tests/conformance"
+  if [ -z ${CONFORMANCE_RESULTS_DIR+x} ]; then
+    echo "Error: CONFORMANCE_RESULTS_DIR is not set, exiting..."
+    exit 1
+  fi
+
+  export CONFORMANCE_RESULTS_DIR="$PWD/$CONFORMANCE_RESULTS_DIR"
+  export RELEASE_TARBALL="$PWD/$KUBO_DEPLOYMENT_DIR/kubo-release.tgz"
+  ginkgo -progress -v "$GOPATH/src/tests/conformance"
+}
+
+main
